52026c5c07f26830bdea71df9d88b75214de665f
Rynte <Podreju.dev@gmail.com>
2023-12-07 23:53:54 +0300

    Добавьте файлы проекта.

---
+++ RemoteDesktop/Models/UserClientFolder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RemoteDesktop.Models
+{
+    internal class UserClientFolder
+    {
+        public string Name { get; set; }
+
+        public ObservableCollection<UserClient> Clients { get; set; }
+    }
+}
---
+++ RemoteDesktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using RemoteDesktop.Models;
+using RemoteDesktop.ViewModels.Base;
+
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace RemoteDesktop.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _status = string.Empty;
+
+        public MainWindowViewModel()
+        {
+            ClientFolders = new ObservableCollection<UserClientFolder>(Enumerable
+                .Range(1, 10)
+                .Select(x => new UserClientFolder()
+                {
+                    Name = "Folder " + x,
+                    Clients = new ObservableCollection<UserClient>(Enumerable
+                    .Range(1, 3)
+                    .Select(y => new UserClient()
+                    {
+                        Name = "Client " + y
+                    }))
+                }));
+        }
+
+        #region Fields
+
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
+
+        public ObservableCollection<UserClientFolder> ClientFolders { get; }
+
+        #endregion Fields
+    }
+}
